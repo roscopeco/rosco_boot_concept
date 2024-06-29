c7a198d2abee01eaec9604c519270ed804cfcf1b
Ross Bamford <roscopeco@gmail.com>
2024-06-30 00:32:57 +0100

    Just convert binary image to C for sanity

--- welcome/kmain.c
+++ welcome/kmain.c
@@ -15,8 +15,13 @@
  */
 
 #include "xosera_m68k_api.h"
+#include "interlace.h"
 #include "pcx.h"
 #include "dprint.h"
+
+#include "splash.h"
+
+#define COPPER_LACE
 
 //volatile xreg_t * const xosera_ptr = (volatile xreg_t * const)0xf80060;
 
@@ -24,11 +29,7 @@ extern void *_image_start;
 extern void *_image_end;
 
 void kmain() {
-    uint32_t buf_start = (uint32_t)&_image_start;
-    uint32_t buf_size  = (uint32_t)&_image_end - (uint32_t)&_image_start;
-    uint8_t *buf = (uint8_t*)buf_start;
-
-    dprintf("Image is at 0x%08x (%d bytes)\n", buf_start, buf_size);
+    dprintf("Image is at 0x%08x (%d bytes)\n", splash_data, splash_data_len);
 
     xv_prep();
 
@@ -36,12 +37,24 @@ void kmain() {
     bool success = xosera_init(XINIT_CONFIG_848x480);
     dprintf("%s\n", success ? "succeeded" : "FAILED" /*, xv_reg_getw(vidwidth), xv_reg_getw(vidheight)*/);
 
-    xreg_setw(PA_GFX_CTRL, 0x0075);     /* 320x240 8bpp */
+    xreg_setw(PA_GFX_CTRL, 0x0065);     /* 320x240 8bpp */
     xreg_setw(PA_DISP_ADDR, 0x0000);
     xreg_setw(PA_LINE_LEN, 424 >> 1);
     xm_setw(WR_INCR, 0x0001);
 
-    show_pcx(buf_size, buf);
+    show_pcx(splash_data_len, splash_data);
+
+#   ifdef COPPER_LACE
+    // load copper program
+    xmem_setw_next_addr(interlace_start);
+    for (uint8_t i = 0; i < interlace_size; i++)
+    {
+        xmem_setw_next(interlace_bin[i]);
+    }
+
+    xreg_setw(COPP_CTRL, 0x8000);    
+    dprintf("Copper initialized\n");
+#   endif
 
     while (true) {}
 }
